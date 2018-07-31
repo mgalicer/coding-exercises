30eb742b31d3fdeb2c95aa58ea46ec129de41b82
marigalicer <marigalicer@gmail.com>
2018-07-30 20:40:19 -0400

    initial solution

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,11 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      StringBuilder reversedStr = new StringBuilder(inputString.length());
+      for(int i = inputString.length() - 1; i >= 0; i--) {
+        reversedStr.append(inputString.charAt(i));
+      }
+      return reversedStr.toString();
     }
-  
+
 }
