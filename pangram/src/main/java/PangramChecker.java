3324a34be36a739a16c294ac0df443dcf4f8e6b3
marigalicer <marigalicer@gmail.com>
2018-09-11 12:01:38 -0400

    refactor

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,32 +1,35 @@
 import java.util.HashMap;
 
 public class PangramChecker {
+    private static HashMap<Character, Boolean> letters() {
+      // initialize hashtable with letters A-Z and a count of 0
+      HashMap<Character, Boolean> letters = new HashMap<>();
+      for (char c = 'A'; c <= 'Z'; c++) {
+        letters.put(c, false);
+      }
+
+      return letters;
+    }
 
     public boolean isPangram(String input) {
       // if input is less than 26 chars, don't bother.
       if(input.length() < 26) return false;
 
-      // initialize hashtable with letters A-Z and a count of 0
-      HashMap<Character, Boolean> letterPresence = new HashMap<>();
-      for (char c = 'A'; c <= 'Z'; c++) {
-        letterPresence.put(c, false);
-      }
+      HashMap<Character, Boolean> letters = letters();
       // pansum variable will be updated as we update letter counts - so if it reaches 26, then iteration can stop.
       int panSum = 0;
-      int count = 0;
 
-      while(count < input.length()) {
+      for(int i = 0; i < input.length(); i++) {
         // if character is a letter, format it and update HashMap and sum
-        if(Character.isLetter(input.charAt(count))) {
-          char letter = Character.toUpperCase(input.charAt(count));
-          if(letterPresence.get(letter) == false) {
-            letterPresence.put(letter, true);
+        if(Character.isLetter(input.charAt(i))) {
+          char letter = Character.toUpperCase(input.charAt(i));
+          if(letters.get(letter) == false) {
+            letters.put(letter, true);
             panSum++;
           }
         }
         // stop iterating, we have all letters of alphabet
         if(panSum == 26) return true;
-        count++;
       }
 
       return false;
